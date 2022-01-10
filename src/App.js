aa60473e1f62f8a195d9ec34db49c86171cf4ad1
sangboking <cowg001@naver.com>
2022-01-10 21:46:35 +0900

    Route 수정

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home/>}/>
-        <Route path="/movie/:id" element={<Detail/>}/>
+        <Route path={process.env.PUBLIC_URL+"/movie/:id"} element={<Detail/>}/>
       </Routes>
     </Router>
   )
